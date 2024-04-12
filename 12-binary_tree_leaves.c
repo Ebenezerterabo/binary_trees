56be1f048cbe30edb058b26a819b80f8f10dedbc
Ebenezer terabo <teraboebenezer@gmail.com>
2024-04-12 03:04:03 +0000

    Task 12 solved

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - A function that measures the size of a binary tree
+ * binary_tree_leaves - A function that measures the size of a binary tree
  * @tree: Pointer to the root node
  *
  * Return: 0 if tree is NULL
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
@@ -15,5 +15,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
